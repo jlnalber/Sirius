ac785f9a78445c23b7d868e552308cec09063ee4
Julian Alber <alber.julian@outlook.com>
2022-02-13 15:04:29 +0100

    restore branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FachComponent } from './faecher/fach/fach.component';
 import { FaecherComponent } from './faecher/faecher.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'faecher/:id',
     component: FachComponent
+  },
+  {
+    path: 'whiteboard',
+    component: WhiteboardComponent
   },
   {
     path: '**',
